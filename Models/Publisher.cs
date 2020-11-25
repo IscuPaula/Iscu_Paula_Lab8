f302023f1161526bafe6efa8ee49b50adde78d57
PAULA-IULIANA ISCU <iscu.paula165150@student.ubbcluj.ro>
2020-11-25 15:33:40 +0200

    Add project files.

---
+++ Models/Publisher.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace Iscu_Paula_Lab8.Models
+{
+    public class Publisher
+    {
+        public int ID { get; set; }
+        public string PublisherName { get; set; }
+        public ICollection <Book> Books { get; set; } //navigation property
+    }
+}
